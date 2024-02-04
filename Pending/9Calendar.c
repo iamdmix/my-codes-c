4c400bb835babaac5ec0daa908bf52e2eb4cbbb4
“Dharmik <dharmikvs26@gmail.com>
2024-02-04 15:28:49 +0530

    Tough programs :(

---
+++ Pending/9Calendar.c
@@ -0,0 +1,53 @@
+//To print the calendar of a month given the number of days and the first day of the month.
+#include<stdio.h>
+
+int main()
+{
+    //To input the details.
+    int n,d,d1=0;
+    printf("Enter the number of days in the month: ");
+    scanf("%d", &n);
+    printf("\nEnter the first day of the month (1--> Sun and 7--> Sat): ");
+    scanf("%d", &d);
+    printf("S M T W T F S\n");
+    // switch(d)
+    // {
+    //     case 1: for(int i =1; i<=7;i++)
+    //     {
+    //         printf("%d",i);
+    //     }
+    //     case 2: for(int i =d; i<=7;i++)
+    //     {
+    //         printf("%d",i);
+    //     }
+    // }
+    if(d>1)
+    {
+        for(int i=0; i < (2*d-2) ;i++)
+        {
+            printf(" ");
+        }
+    }
+    else if (d==7)
+    {
+        printf("\n");
+    }
+    for(int i=1; i<= 8-d; i++)
+    {
+        if(i>7)
+        {
+            break;
+        }
+        printf("%d ",i);
+        d1++;
+    }
+    printf("\n");
+    int d2 = d1;
+    for(int i = d1+1; i< d1+8; i++)
+    {
+        printf("%d ",i);
+        d2++;
+    }
+    return 0;
+
+}
